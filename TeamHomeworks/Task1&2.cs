d0615aa98fb17f59b86bedf1423bdac1f340e17d
fireflyflyfly <tgnstujh.347@gmail.com>
2019-04-29 12:02:33 +0300

    Main part of task 3 done.

--- TeamHomeworks/Task1&2.cs
+++ TeamHomeworks/Task1&2.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using ProgramExtentions;
+using ProgramExtentions2;
 namespace TeamHomeworks
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ProgramExtentions.Extentions.CalculateSquareArea();
-            ProgramExtentions.Extentions.CircleArea();
-            ProgramExtentions.Extentions.CircleInYourSquare();
-            ProgramExtentions.Extentions.SquareInYourCircle();
+            ProgramExtentions2.Task3.RangeCreation();
+            //ProgramExtentions.Extentions.CalculateSquareArea();
+            //ProgramExtentions.Extentions.CircleArea();
+            //ProgramExtentions.Extentions.CircleInYourSquare();
+            //ProgramExtentions.Extentions.SquareInYourCircle();
         }
     }
 }
---
+++ TeamHomeworks/Task3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace ProgramExtentions2
+{
+    public class Task3
+    {
+       public static int rangeStart;
+       public static int rangeFinish;
+       public static List<int> finalArray = new List<int>();
+
+        public static void RangeCreation()
+        {
+            Console.WriteLine("Please enter the first number of your range:");
+            rangeStart = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter the last number of your range:");
+            rangeFinish = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = rangeStart; i <= rangeFinish; i++)
+            { 
+                if ((i % 3 == 0) && (i % 5 != 0))
+                {
+                    finalArray.Add(i);
+                }
+            }
+            finalArray.ForEach(i => Console.WriteLine("{0} ", i));
+        }
+
+    }
+}
