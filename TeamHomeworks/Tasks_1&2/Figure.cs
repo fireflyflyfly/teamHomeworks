824f8edd709b92fe0df66d7099501333d710074e
fireflyflyfly <tgnstujh.347@gmail.com>
2019-06-01 15:49:55 +0300

    updated folder names

---
+++ TeamHomeworks/Tasks_1&2/Figure.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace TeamHomeworks.Task1
+{
+    public abstract class Figure
+    {
+        public const double PI = 3.14159265359;
+        public abstract double CalculateArea();
+
+        public static double GetFigureDimensions()
+        {
+            int i = 3;
+            double x = 0;
+            while (i > 0) 
+            {
+                if (double.TryParse(Console.ReadLine(), out x) && x > 0)
+                {
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine("\nOnly gigits equal or higer than 1 are allowed. Please try again. You have " + (i-1) + " attempts left.");
+                    i--; 
+                }
+            }
+
+            if (i == 0) 
+            { 
+                x = Randomizer.SetRandomValue();
+                Console.WriteLine("\nYou are out of attempts. The value has been automatically set to :" + x);
+            }
+            return x;
+        }
+
+        public static Figure CreateCircle() 
+        {
+            Console.WriteLine("\nPlease nter the radius of your circle:");
+            Circle circle = new Circle(Figure.GetFigureDimensions());
+            return circle; 
+        }
+
+        public static Figure CreateSquare() 
+        {
+            Console.WriteLine("\nPlease nter the length of the side of your square:");
+            Square square = new Square(Figure.GetFigureDimensions());
+            return square; 
+        }
+    }
+}
---
+++ TeamHomeworks/Tasks_1&2/Randomizer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TeamHomeworks.Task1
+{
+    public class Randomizer
+    {
+        public static double SetRandomValue()
+        {
+            Random random = new Random();
+            return random.NextDouble() * (5 - 0.5) + 0.5;
+        }
+    }
+}
